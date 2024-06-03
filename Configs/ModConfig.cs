66a677c53cc13778c6b38edf9d8fdf1f63f06acf
Moosy <70246651+Moosyu@users.noreply.github.com>
2024-06-03 16:00:56 +1200

    added disabling pregnancy

---
+++ Configs/ModConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace SurpriseBaby1
+{
+    public class ModConfig
+    {
+        public bool PregnancyAllowed { get; set; } = true;
+    }
+}
--- ModEntry.cs
+++ ModEntry.cs
@@ -8,35 +8,77 @@ namespace SurpriseBaby1
 {
     public class ModEntry : Mod
     {
-
+        private static ModConfig Config;
         int pregnantNow = 0;
 
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Display.MenuChanged += this.OnMenuChanged;
-            helper.Events.GameLoop.DayStarted += this.OnDayStarted;
+            helper.Events.Display.MenuChanged += OnMenuChanged;
+            helper.Events.GameLoop.DayStarted += OnDayStarted;
+            helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+            Config = Helper.ReadConfig<ModConfig>();
         }
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+
+            // register mod
+            configMenu.Register(
+                mod: ModManifest,
+                reset: () => Config = new ModConfig(),
+                save: () => Helper.WriteConfig(Config)
+            );
+
+            configMenu.AddSectionTitle(
+                mod: ModManifest,
+                text: () => "Uncheck this box to disable pregnancy",
+                tooltip: null
+            );
+
+            // add some config options
+            configMenu.AddBoolOption(
+                mod: ModManifest,
+                name: () => "Pregnancy",
+                tooltip: () => "Check the box for suprise pregnancy, uncheck the box to never be asked about kids.",
+                getValue: () => Config.PregnancyAllowed,
+                setValue: value => Config.PregnancyAllowed = value
+            );
+
+        }
+
+
         private void OnDayStarted(object sender, DayStartedEventArgs e)
         {
-            if (pregnantNow == 1) {
+            if (pregnantNow == 1)
+            {
                 Game1.drawObjectDialogue("You and your partner are expecting!");
                 pregnantNow = 0;
             }
         }
 
-        public void OnMenuChanged(object sender, MenuChangedEventArgs e)
-        {
-            if (Game1.farmEvent is QuestionEvent && this.Helper.Reflection.GetField<int>(Game1.farmEvent, "whichQuestion", true).GetValue() == 1)
-            {
-                if (e.NewMenu is DialogueBox dialogue)
-                {
-                    this.Monitor.Log("Pregnant", LogLevel.Info);
-                    Response yes = new("Yes", "Yes");
-                    Game1.currentLocation.answerDialogue(yes);
+        public void OnMenuChanged(object sender, MenuChangedEventArgs e) {
+            if (Game1.farmEvent is QuestionEvent && Helper.Reflection.GetField<int>(Game1.farmEvent, "whichQuestion", true).GetValue() == 1) {
+                if (e.NewMenu is DialogueBox dialogue) {
+                    if (Config.PregnancyAllowed == true) {
+                        PregnancyAllowedMethod("Pregnant", "Yes");
+                        pregnantNow++;
+                    }
+                    else {
+                        PregnancyAllowedMethod("Pregnancy cancelled", "Not");
+                    }
                     dialogue.closeDialogue();
-                    pregnantNow++;
                 }
             }
         }
+
+        //not particularly necessary but i really like functions idk (even though this isnt a function bc its not returning anything lol)
+        public void PregnancyAllowedMethod(string preg, string yon) {
+            Monitor.Log(preg, LogLevel.Info);
+            Response yes = new(yon, yon);
+            Game1.currentLocation.answerDialogue(yes);
+        }
+
     }
 }
