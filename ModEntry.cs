de7f1542ca4ede2da491bd837ecddcc4444de821
Moosy <70246651+Moosyu@users.noreply.github.com>
2024-06-03 12:26:04 +1200

    bazinga

---
+++ ModEntry.cs
@@ -0,0 +1,42 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Events;
+using StardewValley.Menus;
+
+namespace SurpriseBaby1
+{
+    public class ModEntry : Mod
+    {
+
+        int pregnantNow = 0;
+
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Display.MenuChanged += this.OnMenuChanged;
+            helper.Events.GameLoop.DayStarted += this.OnDayStarted;
+        }
+        private void OnDayStarted(object sender, DayStartedEventArgs e)
+        {
+            if (pregnantNow == 1) {
+                Game1.drawObjectDialogue("You and your partner are expecting!");
+            }
+        }
+
+        public void OnMenuChanged(object sender, MenuChangedEventArgs e)
+        {
+            if (Game1.farmEvent is QuestionEvent && this.Helper.Reflection.GetField<int>(Game1.farmEvent, "whichQuestion", true).GetValue() == 1)
+            {
+                if (e.NewMenu is DialogueBox dialogue)
+                {
+                    this.Monitor.Log("Pregnant", LogLevel.Info);
+                    Response yes = new("Yes", "Yes");
+                    Game1.currentLocation.answerDialogue(yes);
+                    dialogue.closeDialogue();
+                    pregnantNow++;
+                }
+            }
+        }
+    }
+}
+
