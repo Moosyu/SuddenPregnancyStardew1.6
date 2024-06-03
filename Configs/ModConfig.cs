81512373cddf82c2c3dc324589b9f3c137e3d210
Moosy <70246651+Moosyu@users.noreply.github.com>
2024-06-04 07:43:44 +1200

    you can disable the notification now

--- Configs/ModConfig.cs
+++ Configs/ModConfig.cs
@@ -3,5 +3,6 @@
     public class ModConfig
     {
         public bool PregnancyAllowed { get; set; } = true;
+        public bool PregnancyNotification { get; set; } = false;
     }
 }
--- ModEntry.cs
+++ ModEntry.cs
@@ -11,15 +11,13 @@ namespace SurpriseBaby1
         private static ModConfig Config;
         int pregnantNow = 0;
 
-        public override void Entry(IModHelper helper)
-        {
+        public override void Entry(IModHelper helper) {
             helper.Events.Display.MenuChanged += OnMenuChanged;
             helper.Events.GameLoop.DayStarted += OnDayStarted;
             helper.Events.GameLoop.GameLaunched += OnGameLaunched;
             Config = Helper.ReadConfig<ModConfig>();
         }
-        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
-        {
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e) {
             var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
             if (configMenu is null)
                 return;
@@ -46,14 +44,29 @@ namespace SurpriseBaby1
                 setValue: value => Config.PregnancyAllowed = value
             );
 
+            configMenu.AddSectionTitle(
+                mod: ModManifest,
+                text: () => "Check this box to enable the pregnancy notification",
+                tooltip: null
+            );
+
+            // add some config options
+            configMenu.AddBoolOption(
+                mod: ModManifest,
+                name: () => "Pregnancy Notification",
+                tooltip: () => "Notifies you when your spouse gets pregnant 14 days in advance.",
+                getValue: () => Config.PregnancyNotification,
+                setValue: value => Config.PregnancyNotification = value
+            );
+
         }
 
 
-        private void OnDayStarted(object sender, DayStartedEventArgs e)
-        {
-            if (pregnantNow == 1)
-            {
-                Game1.drawObjectDialogue("You and your partner are expecting!");
+        private void OnDayStarted(object sender, DayStartedEventArgs e) {
+            if (pregnantNow == 1) {
+                if (Config.PregnancyNotification == true) { //these two are seperated because if not pregnantNow just keeps ticking up and breaks. probably better way to do this but i gotta do homework so ill fix later.
+                    Game1.drawObjectDialogue("You and your partner are expecting!");
+                }
                 pregnantNow = 0;
             }
         }
